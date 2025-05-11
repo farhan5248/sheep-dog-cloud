015949b05026302669c81fa30dad29a7d79ef2c8
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2025-05-11 14:36:38 -0400

    Test build and deploy instructions

--- sheep-dog-dev-svc-maven-plugin/scripts/forward-engineer.bat
+++ sheep-dog-dev-svc-maven-plugin/scripts/forward-engineer.bat
@@ -1,4 +1,4 @@
 cd ..
 call mvn clean
-call mvn org.farhan:sheep-dog-dev-maven-plugin:1.26-SNAPSHOT:uml-to-cucumber-spring -Dtags="%1" -DrepoDir=../sheep-dog-specs/
+call mvn org.farhan:sheep-dog-dev-maven-plugin:1.26-SNAPSHOT:uml-to-cucumber-spring -DrepoDir=../../sheep-dog-qa/sheep-dog-specs/ -Dtags="round-trip"
 cd scripts 

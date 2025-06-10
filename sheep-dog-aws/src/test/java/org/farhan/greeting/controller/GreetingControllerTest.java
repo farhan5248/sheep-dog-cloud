88c62fa2043b91f4cbeda1f57bee9cace23340d5
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2025-06-10 01:46:23 -0400

    Upgraded MockBean to MockitoBean

--- sheep-dog-aws/src/test/java/org/farhan/greeting/controller/GreetingControllerTest.java
+++ sheep-dog-aws/src/test/java/org/farhan/greeting/controller/GreetingControllerTest.java
@@ -25,7 +25,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -35,10 +35,10 @@ public class GreetingControllerTest {
     @Autowired
     private MockMvc mockMvc;
     
-    @MockBean
+    @MockitoBean
     private GreetingRepository greetingRepository;
     
-    @MockBean
+    @MockitoBean
     private GreetingMessageService messageService;
     
     private List<Greeting> mockGreetings;

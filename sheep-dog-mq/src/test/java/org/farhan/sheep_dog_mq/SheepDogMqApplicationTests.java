8dc06c33f70d97df4c9a0ba6d08dec2cee8f4c3a
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2025-06-09 22:09:22 -0400

    Created sheep-dog-mq

---
+++ sheep-dog-mq/src/test/java/org/farhan/sheep_dog_mq/SheepDogMqApplicationTests.java
@@ -0,0 +1,13 @@
+package org.farhan.sheep_dog_mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheepDogMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d595b093d3e808907b35eebf8ea9c9148cbd44be
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2025-09-01 18:43:08 -0400

    Deleting all claude generated files, need to do it all manually :(

--- xtextasciidocplugin.parent/xtextasciidocplugin.ide/src/main/java/org/farhan/dsl/asciidoc/ide/AsciiDocLanguageServerModule.java
+++
@@ -1,9 +0,0 @@
-package org.farhan.dsl.asciidoc.ide;
-
-import org.eclipse.xtext.ide.server.ServerModule;
-
-public class AsciiDocLanguageServerModule extends ServerModule {
-    
-    // Default implementation for now
-    // Future customizations can be added here
-}

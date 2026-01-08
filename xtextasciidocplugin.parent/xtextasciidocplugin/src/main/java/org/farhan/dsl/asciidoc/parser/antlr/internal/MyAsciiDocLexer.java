aadc72a5cbdcfdcfdd9b3d754200849d489067cf
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2026-01-08 12:27:06 -0500

    Update all Xtext project parsers

--- xtextasciidocplugin.parent/xtextasciidocplugin/src/main/java/org/farhan/dsl/asciidoc/parser/antlr/internal/MyAsciiDocLexer.java
+++ xtextasciidocplugin.parent/xtextasciidocplugin/src/main/java/org/farhan/dsl/asciidoc/parser/antlr/internal/MyAsciiDocLexer.java
@@ -11,6 +11,7 @@ public class MyAsciiDocLexer extends InternalAsciiDocLexer {
 
 	// TODO maybe these should all be one flag called escapeKeyword?
 	private boolean nextTokenIsWORD = false;
+	private boolean useDefaultLexer = false;
 
 	public MyAsciiDocLexer() {
 	}
@@ -35,23 +36,32 @@ public class MyAsciiDocLexer extends InternalAsciiDocLexer {
 		return true;
 	}
 
+	public void printNextToken() throws MismatchedTokenException {
+		int i = 0;
+		try {
+			String s = "";
+			while (!Character.isWhitespace(input.LA(i))) {
+				s += Character.toString(input.LA(i));
+				i++;
+			}
+			System.out.println("Token >>>" + s + "<<<");
+		} catch (Exception e) {
+			System.out.println("Error >>>" + input.LA(i) + "<<<");
+		}
+	}
+
 	@Override
 	public void mTokens() throws RecognitionException {
-		// TODO document how to update the lexer in markdown
-		// get the position from input and find the next set of non-space characters
-		// This is a hack, ideally the xtext grammar file needs to be defined better,
-		// when I have more time I'll review the need to do this but for now I figured
-		// it'd be a good example on how to hack the lexer for newbies
-
 		if (isKeyword("----")) {
-			mRULE_RAWTEXT();// ( '----' RULE_EOL ( . )+ '----' RULE_EOL )
+			mRULE_RAWTEXT();
 		} else if (isKeyword("#")) {
 			mRULE_SL_COMMENT();
+		} else if (isKeyword(" ") || isKeyword("\t") || isKeyword("\r")) {
+			mRULE_WS();
 		} else if (isKeyword("\n")) {
 			mRULE_EOL();
 			nextTokenIsWORD = false;
-		} else if (isKeyword(" ") || isKeyword("\t") || isKeyword("\r")) {
-			mRULE_WS();
+			useDefaultLexer = false;
 		} else if (isKeyword("+")) {
 			mT__23();
 		} else if (isKeyword("|===")) {
@@ -68,27 +78,33 @@ public class MyAsciiDocLexer extends InternalAsciiDocLexer {
 		} else if (nextTokenIsWORD) {
 			mRULE_WORD();
 		} else if (isKeyword("Step-Object:")) {
-			mT__10();// 'Object:'
+			mT__10();
 		} else if (isKeyword("Step-Definition:")) {
-			mT__12();// 'Definition:'
+			mT__12();
 		} else if (isKeyword("Step-Parameters:")) {
-			mT__14();// 'Parameters:'
+			mT__14();
 		} else if (isKeyword("Test-Suite:")) {
-			mT__15();// 'Feature:'
+			mT__15();
 		} else if (isKeyword("Test-Setup:")) {
-			mT__16();// 'Background:'
+			mT__16();
 		} else if (isKeyword("Test-Case:")) {
-			mT__17();// 'Scenario:'
+			mT__17();
 		} else if (isKeyword("Test-Data:")) {
-			mT__18();// 'Examples:'
+			mT__18();
 		} else if (isKeyword("Given:")) {
-			mT__19();// 'Given'
+			mT__19();
+			useDefaultLexer = true;
 		} else if (isKeyword("When:")) {
-			mT__20();// 'When'
+			mT__20();
+			useDefaultLexer = true;
 		} else if (isKeyword("Then:")) {
-			mT__21();// 'Then'
+			mT__21();
+			useDefaultLexer = true;
 		} else if (isKeyword("And:")) {
-			mT__22();// 'And'
+			mT__22();
+			useDefaultLexer = true;
+		} else if (useDefaultLexer) {
+			super.mTokens();
 		} else {
 			mRULE_WORD();
 		}

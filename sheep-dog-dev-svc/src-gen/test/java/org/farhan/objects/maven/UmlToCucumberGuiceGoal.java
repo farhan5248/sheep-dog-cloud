21935a9470a3889ee29b731144a42671801789d2
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2026-03-11 17:03:30 -0400

    Review negativeTest flag

--- sheep-dog-dev-svc/src-gen/test/java/org/farhan/objects/maven/UmlToCucumberGuiceGoal.java
+++ sheep-dog-dev-svc/src-gen/test/java/org/farhan/objects/maven/UmlToCucumberGuiceGoal.java
@@ -3,5 +3,6 @@ package org.farhan.objects.maven;
 import java.util.HashMap;
 
 public interface UmlToCucumberGuiceGoal {
+
     public void setExecuted(HashMap<String, String> keyMap);
 }
--- sheep-dog-dev-svc/src-gen/test/java/org/farhan/objects/maven/UmlToCucumberSpringGoal.java
+++ sheep-dog-dev-svc/src-gen/test/java/org/farhan/objects/maven/UmlToCucumberSpringGoal.java
@@ -3,5 +3,6 @@ package org.farhan.objects.maven;
 import java.util.HashMap;
 
 public interface UmlToCucumberSpringGoal {
+
     public void setExecuted(HashMap<String, String> keyMap);
 }
